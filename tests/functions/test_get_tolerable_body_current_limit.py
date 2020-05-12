c3fd7e9656271292a11475ca2050dc86960ab5f4
Raphael Santana <uroboros.phael@gmail.com>
2020-05-11 23:38:13 -0300

    Testing get_tolerable_body_current_limit function.

---
+++ tests/functions/test_get_tolerable_body_current_limit.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from grounding.functions.get_tolerable_body_current_limit import get_tolerable_body_current_limit
+
+
+class GetTolerableBodyCurrentLimitTest(TestCase):
+
+    def test_sample_from_textbook(self):
+        exposure_time = 0.5
+        current_limit = get_tolerable_body_current_limit(
+            exposure_time,
+            use_50kgs_model=False
+        )
+        self.assertAlmostEquals(current_limit, 0.2220315292925759)
+
+    def test_sample_from_textbook_with_50kgs(self):
+        exposure_time = 0.5
+        current_limit = get_tolerable_body_current_limit(
+            exposure_time,
+            use_50kgs_model=True
+        )
+        self.assertAlmostEquals(current_limit, 0.16404877323527903)
+
+    def test_small_exposure_time(self):
+        with self.assertRaises(ValueError):
+            current_limit = get_tolerable_body_current_limit(0.029)
+            self.assertIsNone(current_limit)
+
+    def test_large_exposure_time(self):
+        with self.assertRaises(ValueError):
+            current_limit = get_tolerable_body_current_limit(3.1)
+            self.assertIsNone(current_limit)
